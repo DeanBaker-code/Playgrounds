fc298d35baa12e79baab178d8fc672a3d949d43c
DeanBaker-code <77198096+DeanBaker-code@users.noreply.github.com>
2021-03-06 03:14:12 -0500

    Update 3-Checking This AND That.swift

--- Code 1/5-Logical Operators/3-Checking This AND That.swift
+++ Code 1/5-Logical Operators/3-Checking This AND That.swift
@@ -1,1 +1,20 @@
-//
+for i in 1 ... 7 {
+    moveForward()
+    if isOnGem && isBlockedLeft{
+        collectGem()
+        turnRight()
+        getSwitch()
+    }
+    collectGem()
+}
+func getSwitch() {
+    moveForward()
+    moveForward()
+    toggleSwitch()
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+    turnRight() 
+}
+get humans on mars dedicated to elon musk
