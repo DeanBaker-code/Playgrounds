d7dce117ad3f86865fb7baa408fd213531638f22
DeanBaker-code <77198096+DeanBaker-code@users.noreply.github.com>
2021-03-06 03:08:30 -0500

    Update 5-Logical Labyrinth.swift

--- Code 1/5-Logical Operators/5-Logical Labyrinth.swift
+++ Code 1/5-Logical Operators/5-Logical Labyrinth.swift
@@ -1,1 +1,32 @@
-//
+func turnAround() {
+    turnLeft()
+    turnLeft()
+}
+
+func goAndGetGem(){
+turnRight()
+moveForward()
+moveForward()
+turnAround()
+collectGem()
+moveForward()
+moveForward()
+turnRight()
+}
+
+
+for i in 1 ... 8 {
+    moveForward()
+    if isOnGem && isOnClosedSwitch{
+    toggleSwitch()
+    collectGem()
+        goAndGetGem()
+    }
+    if isBlocked || isOnClosedSwitch{
+        toggleSwitch()
+        turnLeft()
+    }
+}
+turnAround()
+moveForward()
+collectGem()
