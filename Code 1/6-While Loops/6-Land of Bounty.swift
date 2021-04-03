3fde8b134bab7c71cfc2b7dd36f874673190bb03
DeanBaker-code <77198096+DeanBaker-code@users.noreply.github.com>
2021-04-03 10:59:18 -0400

    complete

--- Code 1/6-While Loops/6-Land of Bounty.swift
+++ Code 1/6-While Loops/6-Land of Bounty.swift
@@ -1,1 +1,31 @@
 //
+for i in 1 ... 5 {
+    moveForward()
+    while isOnClosedSwitch {
+        toggleSwitch()
+    }}
+turnRight()
+moveForward()
+turnRight()
+for i in 1 ... 5 {
+    moveForward()
+    while isOnGem {
+        collectGem()
+    }
+}
+turnLeft()
+moveForward()
+turnLeft()
+for i in 1 ... 5 {
+    moveForward()
+    while isOnClosedSwitch {
+        toggleSwitch()
+    }}
+
+
+/*solve row one
+ solve switches
+ solve row two
+ solving gems
+solve row three*/
+//solving switches
