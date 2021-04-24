7df0c6247d2a657b875a2a0aa28b199b98ffa2e2
DeanBaker-code <77198096+DeanBaker-code@users.noreply.github.com>
2021-04-24 11:08:57 -0400

    complete
    fgfgffgfgfcghgfcfgfgh

--- Code 1/6-While Loops/8-Random Rectangles.swift
+++ Code 1/6-While Loops/8-Random Rectangles.swift
@@ -1,1 +1,20 @@
 //
+while !isBlocked
+{
+    solveRow()
+    turnRight()
+}
+
+func solveRow(){
+    while !isBlocked{
+        moveForward()
+    }
+}
+
+
+toggleSwitch()
+/* get to the switch. 
+ is not blocked keep going forward
+ turn right continue going forward
+ 
+ */
