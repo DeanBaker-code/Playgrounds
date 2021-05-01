b0aca22359387e10d941060199ba02e8644c0746
DeanBaker-code <77198096+DeanBaker-code@users.noreply.github.com>
2021-05-01 11:02:14 -0400

    complete
    gegtg

--- Code 1/6-While Loops/9-You r Always Right.swift
+++ Code 1/6-While Loops/9-You r Always Right.swift
@@ -1,1 +1,21 @@
 //
+/* 
+ while i am not against the wall i need to keep on walking.
+ while im doing that toggle any closed switch.
+ turn right
+ 
+ */
+
+while !isOnGem {
+    jump()
+    if isOnClosedSwitch {
+        toggleSwitch()
+    }
+    while isBlocked {
+        turnRight()
+    }
+}
+
+if isOnGem {
+    collectGem()
+}
