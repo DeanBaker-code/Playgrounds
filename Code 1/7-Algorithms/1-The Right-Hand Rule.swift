a6a70bb33143839b6e2d85b7052b62876eb1c492
DeanBaker-code <77198096+DeanBaker-code@users.noreply.github.com>
2021-05-08 11:13:33 -0400

    complete
    yfgycrgi

--- Code 1/7-Algorithms/1-The Right-Hand Rule.swift
+++ Code 1/7-Algorithms/1-The Right-Hand Rule.swift
@@ -1,1 +1,32 @@
 //
+/*right-hand rule. hug/stay against wall.
+ 
+ while i am not on a switch{
+ while not on gem {
+ use the right hand rule
+ }
+ collect the gem and turn around
+ }
+ stop and toggle the switch
+ 
+ */
+
+func navigateAroundWall() {
+    if isBlockedRight {
+        moveForward()
+    }  else {
+        turnRight()
+        moveForward()
+    }
+}
+while !isOnClosedSwitch{
+    while !isOnGem && !isOnClosedSwitch {
+        navigateAroundWall()
+    }
+    collectGem()
+    turnLeft()
+    turnLeft()
+    
+}
+toggleSwitch()
+
