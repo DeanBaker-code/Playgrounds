a7665ce1a365c824a893d27a92e9da8187741401
DeanBaker-code <77198096+DeanBaker-code@users.noreply.github.com>
2021-05-15 10:44:12 -0400

    complete
    geujfyejlaknwg

--- Code 1/7-Algorithms/2-Adjusting Your Algorithm.swift
+++ Code 1/7-Algorithms/2-Adjusting Your Algorithm.swift
@@ -1,1 +1,23 @@
 //
+func navigateAroundWall() {
+    if isBlockedRight{
+        if isBlocked {
+            turnLeft()
+        }
+        
+        moveForward()
+    }  else {
+        turnRight()
+        moveForward()
+    }
+}
+    
+while !isOnClosedSwitch {
+    navigateAroundWall()
+    if isOnGem {
+        collectGem()
+        turnLeft()
+        turnLeft()
+    }
+}
+toggleSwitch()
