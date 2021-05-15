79bde54edc682255090a1b956853c82b3d6eb74b
DeanBaker-code <77198096+DeanBaker-code@users.noreply.github.com>
2021-05-15 11:11:42 -0400

    complete
    jdnfhclurvk

--- Code 1/7-Algorithms/3-Conquering a Maze.swift
+++ Code 1/7-Algorithms/3-Conquering a Maze.swift
@@ -1,1 +1,63 @@
 //
+/*
+func theImpossible() {
+    moveForward()
+    moveForward()
+    moveForward()
+    turnLeft()
+    moveForward()
+    moveForward()
+    moveForward()
+    turnLeft()
+    moveForward()
+    moveForward()
+    moveForward()
+    turnRight()
+    moveForward()
+    moveForward()
+    turnRight()
+    moveForward()
+    moveForward()
+    moveForward()
+    moveForward()
+    moveForward()
+    turnRight()
+    moveForward()
+    moveForward()
+    moveForward()
+    turnLeft()
+    moveForward()
+    moveForward()
+    turnLeft()
+    moveForward()
+    moveForward()
+    turnLeft()
+    moveForward()
+    turnRight()
+    moveForward()
+    moveForward()
+    collectGem()}
+
+
+
+theImpossible()
+ */
+
+func navigateAroundWall() {
+    if isBlockedRight{
+        if isBlocked {
+            turnLeft()
+        }
+        
+        moveForward()
+    }  else {
+        turnRight()
+        moveForward()
+    }
+}
+
+
+while !isOnGem {
+    navigateAroundWall()
+}
+collectGem()
