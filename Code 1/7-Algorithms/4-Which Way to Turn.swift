b64a39a43945a5866ce83d09f5053a6112b49d7b
DeanBaker-code <77198096+DeanBaker-code@users.noreply.github.com>
2021-05-22 11:30:39 -0400

    complete
    9yfxri34cngero

--- Code 1/7-Algorithms/4-Which Way to Turn.swift
+++ Code 1/7-Algorithms/4-Which Way to Turn.swift
@@ -1,1 +1,64 @@
 //
+/* 
+ 
+ while im not at the gem continue solving the puzzle{
+
+ Pattern1
+ while im not on a switch {
+ moveforward
+ }
+ if im !Blocked {
+ toggle switch
+ turn right
+ } else{
+ toggle switch and turn left
+ }
+
+
+ 
+
+ 
+ 
+ 
+ */
+func pattern1() {
+    if !isOnClosedSwitch {
+        moveForward()
+    }else {
+        if !isBlocked{
+            toggleSwitch()
+            turnRight()
+        }else {
+            toggleSwitch()
+            turnLeft()
+        }
+        
+    }
+    
+}
+
+
+while !isOnGem {
+    pattern1()
+}
+
+collectGem()
+
+
+/*
+moveForward()
+while !isOnClosedSwitch {
+    moveForward()
+    
+    
+}
+
+    if isOnClosedSwitch && !isBlocked {
+        toggleSwitch()
+        turnRight()
+    }else {
+        toggleSwitch()
+        turnLeft()
+    }
+    
+ */
