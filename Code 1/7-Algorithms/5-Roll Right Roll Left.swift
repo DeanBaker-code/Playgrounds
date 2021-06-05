0bd2151de57401d64c41a6ae65433e8a6c884a05
DeanBaker-code <77198096+DeanBaker-code@users.noreply.github.com>
2021-06-05 11:12:44 -0400

    Update 5-Roll Right Roll Left.swift

--- Code 1/7-Algorithms/5-Roll Right Roll Left.swift
+++ Code 1/7-Algorithms/5-Roll Right Roll Left.swift
@@ -1,1 +1,36 @@
 //
+/* (move forwrd and then turn right) 
+ while you are not blocked repeat below{
+ if im on a gem collect the gem
+ if im not on a gem just move forward
+ }
+ */
+
+
+
+for i in 1 ... 2 {
+
+    while !isBlocked {
+        if isOnGem{
+            collectGem()
+        }else{
+            moveForward()
+        }
+    }
+    turnLeft()
+    moveForward()
+    turnLeft()
+    while !isBlocked  {
+        if isOnClosedSwitch {
+            toggleSwitch()
+        }else{
+            moveForward()
+            if isBlocked {
+                moveForward()
+                turnRight()            }
+            
+        }
+        
+    }
+}
+
